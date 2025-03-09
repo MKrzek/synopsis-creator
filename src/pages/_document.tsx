48682cb455cae4e85ac383282823e9ba9bc0201e
MKrzek <mkrzek@googlemail.com>
2025-03-09 19:56:25 +0000

    feat: cleanup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#f3eded" />
+      </Head>
       <body>
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );
